d9bf60e5ae2deec0164ca94f7c2a501e151e5a73
kaspar.lund <kl@innopro.dk>
2019-05-19 10:49:17 +0200

    minimal model

--- code.py
+++ code.py
@@ -8,8 +8,9 @@ import cv2
 import brotli
 from PIL import Image, ImageDraw
 from io import BytesIO,SEEK_SET
-
+import pickle
 import gc
+from core_ai.lib.data import *
 
 #convert strokes to an image
 min_color        = 10 #so that we can see the line
@@ -33,7 +34,23 @@ def strokes2image(str_strokes:str, size:int=256, lw:int=6, linetype=cv2.LINE_8):
         
     return img
 
-def convert_strokes2image(drawings, size, line_width, linetype, compressed_drawings):
+def drawing2image_nomalized(side_size:int, line_width:int, linetype:int, compressed_drawings:bool,
+                     image_mean:float=0, image_sd:float=1):
+    image_mean, image_sd = image_mean*255, image_sd * 255
+    def _inner(drawing): 
+        im  = strokes2image( brotli.decompress(drawing).decode("utf-8"), size=side_size, lw=line_width, linetype=linetype)
+        im = (np.asarray( im, dtype=np.float32)-image_mean ) / image_sd 
+        return im.reshape(-1,side_size,side_size)
+    return _inner
+
+def drawing2image(drawing, size, line_width, linetype, compressed_drawings):
+    if compressed_drawings:
+        im = strokes2image( brotli.decompress(drawing).decode("utf-8"), size=size, lw=line_width, linetype=linetype)
+    else: 
+        im = strokes2image( drawing, size=size, lw=line_width, linetype=linetype)
+    return im
+
+def drawings2images(drawings, size, line_width, linetype, compressed_drawings):
     "convert strokes to images"
     ims = []
     for drawing in drawings : 
@@ -44,7 +61,7 @@ def convert_strokes2image(drawings, size, line_width, linetype, compressed_drawi
         ims.append( im )
     return ims
 
-    #compressing and decompressing images
+#compressing and decompressing images
 def convert_strokes2webp(drawings, size, line_width):
     "convert strokes to images"
     ims = []
@@ -62,35 +79,35 @@ def decode_images(imgs:list):
             ims_de.append( np.asarray(Image.open(byteIO).convert("L")) )            
     return ims_de
 
-def loadall(pickle_file):
-    with open(pickle_file, "rb") as f:
-        while True:
-            try:
-                yield pickle.load(f)
-            except EOFError:
-                break
 
-def readallImages(pickle_file ):
-    "return a list of images compressed and save by merge_stroke_files"
-    imgs = []
-    for imgs_section in loadall(pickle_file):
-        imgs.extend( imgs_section )
-        gc.collect()    
-    return imgs
+class NormalizedDataset(Dataset):
+    def __init__(self, x, y, qd2norm_image): 
+        self.x,self.y, self.qd2norm_image = x,y,qd2norm_image
+    def __getitem__(self, i): 
+        return  self.qd2norm_image(self.x[i]), int(self.y[i])
 
-# Numericalization of labels
-# We try to get then same validation ratio for each label
-def split_train_valid_in_place( df, ix_train_valid_col:int, valid_ratio:float):
-    # increase valid_ratio is a bit high due to truncation that happens when doing randomization in groups
-    valid_ratio = 1.06*valid_ratio
- 
-    k=0
-    for n,g in df.groupby("word_code"):
-        if len(g) > 1: 
-            ix_valid = np.random.randint(0, len(g), size=int( len(g)*valid_ratio+.5) )
-            df.iloc[g.index[ix_valid], ix_train_valid_col] = False 
+class GDDataset(Dataset):
+    def __getitem__(self, i): 
+        return  self.x[i], int(self.y[i])
+
+
+def readDatasets(train_ds_file:Path, valid_ds_file:Path):
+    with train_ds_file.open("rb") as file:
+        train_ds = pickle.load(file)
+    with valid_ds_file.open("rb") as file:
+        valid_ds = pickle.load(file)
     gc.collect()
-    valid_ratio_measured = (1- sum(df["train_valid"]) / len(df) )
-    print(f"Specificied validation ratio:{valid_ratio} measured ration:{valid_ratio_measured}" )
+    return train_ds, valid_ds
 
-    
+
+def calc_x_mean( ds:Dataset, qd2image ): 
+    x_mean   = 0
+    for x,y in progress_bar(ds):
+        x_mean += qd2image(x).flatten().mean()
+    return x_mean / len(train_ds)
+
+def calc_x_sd(ds:Dataset, x_mean:float, gd2image):
+    x_sd     = 0
+    for x,y in progress_bar(ds):
+        x_sd += np.power( gd2image(x).flatten() - x_mean, 2).mean()
+    return math.sqrt( x_sd / len(train_ds) )
--- graphics.py
+++ graphics.py
@@ -3,14 +3,23 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt 
 import numpy as np
 import math
+from code import *
 
 #graphics
-def plot_images( ims, cmap=cm.Greys, max_rows_cols=10 ):
+def plot_images( ims, cmap=cm.Greys, max_rows_cols=10, figsize=(12, 12) ):
     n        = max(2,min(max_rows_cols, int(math.sqrt(len(ims))) ))
-    fig, axs = plt.subplots(nrows=n, ncols=n, sharex=True, sharey=True, figsize=(12, 12))
+    fig, axs = plt.subplots(nrows=n, ncols=n, sharex=True, sharey=True, figsize=figsize)
     n_figs   = min(len(ims),n**2)
     for i in range(n_figs):
         ax = axs[i // n, i % n]
         ax.imshow( ims[i], cmap=cm.Greys)
         ax.axis('off')
     plt.tight_layout()
+
+
+def draw_images_from_dataset(dataset, size:int, line_width:int, linetype:int, nb=1000, nrows=8, compressed_drawings=True, figsize=(12, 12)):
+    print("reading and converting compressed drawings to images")
+    compressed = [dataset.x[i] for i in np.random.randint(0,len(dataset.x),nb) ]
+    ims = drawings2images(compressed, size, line_width, linetype, compressed_drawings) 
+    print(f"plot images : {len(ims)}")
+    plot_images( ims, cm.Greys, max_rows_cols=nrows, figsize=figsize )
